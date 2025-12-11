d263f444b2e8a4503a2234bb3a5fa73f69c82fe6
harryngaishophouse <harryngkokjing.aishophouse@gmail.com>
2025-12-11 22:33:54 +0800

    Implement: handleMute() and handleUnmute() commands

--- telegram/client.go
+++ telegram/client.go
@@ -21,9 +21,9 @@ func HandleCommand(ctx context.Context, client *firestore.Client, command string
 	case "/status":
 		err = handleStatus(chatId)
 	case "/mute":
-		err = handleMute(chatId)
+		err = handleMute(ctx, client, chatId)
 	case "/unmute":
-		err = handleUnmute(chatId)
+		err = handleUnmute(ctx, client, chatId)
 	case "/unlink":
 		err = handleUnlink(chatId)
 	case "/help":
@@ -67,11 +67,79 @@ func handleStatus(chatId int64) error {
 	return nil
 }
 
-func handleMute(chatId int64) error {
+func handleMute(ctx context.Context, client *firestore.Client, chatId int64) error {
+	isSetup, err := db.Mute(ctx, client, chatId)
+	
+	var message string = ""
+	if !isSetup {
+		message = MuteBeforeStartErrorMessage
+	} else if err != nil {
+		message = DefaultErrorMessage
+	} else {
+		message = MuteSuccessMessage
+	}
+
+	url := fmt.Sprintf("%v%v", config.TelegramCfg.TELEGRAM_BOT_API_BASE_URL, "sendMessage")
+
+	payload := struct {
+		ChatID    int    `json:"chat_id"`
+		ParseMode string `json:"parse_mode"`
+		Text      string `json:"text"`
+	} {
+		ChatID: int(chatId),
+		ParseMode: "MarkdownV2",
+		Text: message,
+	}
+
+	reqBody, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewReader(reqBody))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
 	return nil
 }
 
-func handleUnmute(chatId int64) error {
+func handleUnmute(ctx context.Context, client *firestore.Client, chatId int64) error {
+	isSetup, err := db.Unmute(ctx, client, chatId)
+	
+	var message string = ""
+	if !isSetup {
+		message = UnmuteBeforeStartErrorMessage
+	} else if err != nil {
+		message = DefaultErrorMessage
+	} else {
+		message = UnmuteSuccessMessage
+	}
+
+	url := fmt.Sprintf("%v%v", config.TelegramCfg.TELEGRAM_BOT_API_BASE_URL, "sendMessage")
+
+	payload := struct {
+		ChatID    int    `json:"chat_id"`
+		ParseMode string `json:"parse_mode"`
+		Text      string `json:"text"`
+	} {
+		ChatID: int(chatId),
+		ParseMode: "MarkdownV2",
+		Text: message,
+	}
+
+	reqBody, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewReader(reqBody))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
 	return nil
 }
 
