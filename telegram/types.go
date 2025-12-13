04044ee9b65ff2e2f183a7890703d8f4e64538e1
IteratorInnovator <harryngkokjing@gmail.com>
2025-12-13 17:47:02 +0800

    Remove: unnecessary fields

--- telegram/types.go
+++ telegram/types.go
@@ -7,28 +7,14 @@ type Update struct {
 
 type Message struct {
     MessageID int             `json:"message_id"`
-    From      *User           `json:"from"`
     Chat      Chat            `json:"chat"`
     Date      int64           `json:"date"`
     Text      string          `json:"text"`
-    Entities  []MessageEntity `json:"entities,omitempty"`
-}
-
-type User struct {
-    ID       int64  `json:"id"`
-    IsBot    bool   `json:"is_bot"`
-    Username string `json:"username"`
 }
 
 type Chat struct {
     ID   int64  `json:"id"`
     Type string `json:"type"`
-}
-
-type MessageEntity struct {
-    Offset int    `json:"offset"`
-    Length int    `json:"length"`
-    Type   string `json:"type"`
 }
 
 type WebHookInfo struct {
