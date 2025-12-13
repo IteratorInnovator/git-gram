617009d6e427cd078fe99088fbc68fd4fe52dd46
IteratorInnovator <harryngkokjing@gmail.com>
2025-12-13 17:44:22 +0800

    Add: functions for fetching user doc

--- db/firestore.go
+++ db/firestore.go
@@ -25,14 +25,15 @@ func SaveChat(ctx context.Context, client *firestore.Client, chat_id int64) erro
 
 	data["chat_id"] = chat_id
 	data["installation_id"] = nil
-	data["mute"] = false
+	data["github_account_username"] = ""
+	data["muted"] = false
 
 	_, err := client.Collection("users").Doc(docId).Create(ctx, data)
 
 	return err
 }
 
-func SaveInstallation(ctx context.Context, client *firestore.Client, chat_id int64, installation_id int64) error {
+func UpdateInstallation(ctx context.Context, client *firestore.Client, chat_id int64, installation_id int64, account_username string) error {
 	var chatId string = strconv.FormatInt(chat_id, 10)
 
 	docRef := client.Collection("users").Doc(chatId)
@@ -42,11 +43,12 @@ func SaveInstallation(ctx context.Context, client *firestore.Client, chat_id int
 		return errors.New("doc does not exist")
 	}
 
-	_, err := docRef.Update(ctx, []firestore.Update{
+	_, err := docRef.Update(ctx, []firestore.Update {
     	{ Path: "installation_id", Value: installation_id },
+		{ Path: "github_account_username", Value: account_username },
 	})
 	if (err != nil) {
-		return errors.New("failed to update installation_id")
+		return errors.New("failed to save installation")
 	}
 	return nil
 }
@@ -62,7 +64,7 @@ func Mute(ctx context.Context, client *firestore.Client, chat_id int64) (bool, e
 	}
 
 	_, err := docRef.Update(ctx, []firestore.Update{
-    	{ Path: "mute", Value: true },
+    	{ Path: "muted", Value: true },
 	})
 	if (err != nil) {
 		return true, errors.New("failed to mute")
@@ -81,7 +83,7 @@ func Unmute(ctx context.Context, client *firestore.Client, chat_id int64) (bool,
 	}
 
 	_, err := docRef.Update(ctx, []firestore.Update{
-    	{ Path: "mute", Value: false },
+    	{ Path: "muted", Value: false },
 	})
 	if (err != nil) {
 		return true, errors.New("failed to unmute")
@@ -89,3 +91,45 @@ func Unmute(ctx context.Context, client *firestore.Client, chat_id int64) (bool,
 	return true, nil
 }
 
+func FetchUserInfo(ctx context.Context, client *firestore.Client, chat_id int64) (string, bool, error) {
+	var chatId string = strconv.FormatInt(chat_id, 10)
+
+	docRef := client.Collection("users").Doc(chatId)
+	snap, _ := docRef.Get(ctx)
+
+	if !snap.Exists() {
+		return "", false, errors.New("user doc does not exist")
+	}
+
+	var u struct {
+		AccountLogin string `firestore:"github_account_username"`
+		Muted        bool   `firestore:"muted"`
+	}
+	err := snap.DataTo(&u)
+	if err != nil {
+		return "", false, err
+	}
+
+	return u.AccountLogin, u.Muted, nil
+}
+
+func FetchInstallationId(ctx context.Context, client *firestore.Client, chat_id int64) (int64, error) {
+	var chatId string = strconv.FormatInt(chat_id, 10)
+
+	docRef := client.Collection("users").Doc(chatId)
+	snap, _ := docRef.Get(ctx)
+
+	if !snap.Exists() {
+		return 0, errors.New("user doc does not exist")
+	}
+
+	var data struct {
+		InstallationId int64 `firestore:"installation_id"`
+	}
+	err := snap.DataTo(&data)
+	if err != nil {
+		return 0, err
+	}
+
+	return data.InstallationId, nil
+}
