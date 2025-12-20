979f5dd776c0583b4c7336015fabf5a905e09e94
IteratorInnovator <harryngkokjing@gmail.com>
2025-12-20 16:37:51 +0800

    Rename events to message_templates

---
+++ github/message_templates/push.go
@@ -0,0 +1,18 @@
+package message_templates
+
+const SingleCommitPush string = `🔔 *New Push to %s*
+
+[%s](%s) pushed 1 commit to ` + "`%s`" + `at %s
+
+Commit: ` + "`%s`" + `
+%s`
+
+
+const MultipleCommitsPush string = `🔔 *New Push to %s*
+
+[%s](%s) pushed %d commits to ` + "`%s`" + `at %s
+
+Latest Commit: ` + "`%s`" + `
+%s`
+
+
