0198fdb2a1b58d161628dd77424b3564b6835c8c
IteratorInnovator <harryngkokjing@gmail.com>
2025-12-20 16:39:52 +0800

    Add logic for handling push event

--- github/handlers.go
+++ github/handlers.go
@@ -1,15 +1,25 @@
 package github
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"strings"
+	"time"
 
+	"github.com/IteratorInnovator/git-gram/config"
+	"github.com/IteratorInnovator/git-gram/github/message_templates"
+	events "github.com/IteratorInnovator/git-gram/github/message_templates"
 	"github.com/gofiber/fiber/v2"
 )
 
 func HandleGitHubWebhookEvent(event string, chatId int64, ctx *fiber.Ctx) error {
+	var err error = nil
+
 	switch (event) {
 		case "push":
-			fmt.Println("push event")
+			err = handlePushEvent(ctx, chatId)
 		case "create":
 			fmt.Println("create event")
 		case "delete":
@@ -19,5 +29,91 @@ func HandleGitHubWebhookEvent(event string, chatId int64, ctx *fiber.Ctx) error 
 		default:
 			fmt.Printf("%v event", event)
 	}
+	return err
+}
+
+func handlePushEvent(ctx *fiber.Ctx, chatId int64) error {	
+	url := fmt.Sprintf("%v%v", config.TelegramCfg.TELEGRAM_BOT_API_BASE_URL, "sendMessage")
+
+	var pushEvent PushEvent
+	err := ctx.BodyParser(&pushEvent)
+	if err != nil {
+		return err
+	}
+
+	keyboardButtons := [][]InlineKeyboardButton {
+		{ 
+			InlineKeyboardButton { 
+				Text: "View Commit", 
+				URL: pushEvent.HeadCommit.URL,
+			},
+			InlineKeyboardButton{
+				Text: "Changes",
+				URL: pushEvent.Compare,
+			},
+		},
+		{ 
+			InlineKeyboardButton { 
+				Text: "Repository", 
+				URL: pushEvent.Repository.HTMLURL,
+			},
+			InlineKeyboardButton{
+				Text: "Branch",
+				URL: pushEvent.Repository.HTMLURL + "/tree/" + formatRef(pushEvent.Ref),
+			},
+		},
+	}
+
+	var messageText string = ""
+	var commitCount int = len(pushEvent.Commits)
+	if commitCount > 1 {
+		messageText = fmt.Sprintf(
+			message_templates.SingleCommitPush,
+			pushEvent.Repository.FullName,
+			pushEvent.Sender.Login,
+			pushEvent.Sender.HTMLURL,
+			formatRef(pushEvent.Ref),
+			shortenSHA(pushEvent.HeadCommit.ID),
+			pushEvent.HeadCommit.Message,
+		)
+	} else {
+		messageText = fmt.Sprintf(
+			message_templates.MultipleCommitsPush,
+			pushEvent.Repository.FullName,
+			pushEvent.Sender.Login,
+			pushEvent.Sender.HTMLURL,
+			commitCount,
+			formatRef(pushEvent.Ref),
+			shortenSHA(pushEvent.HeadCommit.ID),
+			pushEvent.HeadCommit.Message,
+		)
+	}
+	
+
+	payload := struct {
+		ChatID      int                  `json:"chat_id"`
+		ParseMode   string               `json:"parse_mode"`
+		Text        string               `json:"text"`
+		ReplyMarkup InlineKeyboardMarkup `json:"reply_markup"`
+	} {
+		ChatID: int(chatId),
+		ParseMode: "MarkdownV2",
+		Text: messageText,
+		ReplyMarkup: InlineKeyboardMarkup{
+			InlineKeyboard: keyboardButtons,
+		},
+	}
+
+	reqBody, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewReader(reqBody))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
 	return nil
 }
